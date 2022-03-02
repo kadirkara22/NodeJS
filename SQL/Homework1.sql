ad9ea1dc096fe1051cef289d83c936b2fc7259c6
kadirkara22 <kadirkar2204@gmail.com>
2022-03-02 12:41:31 +0300

    homework2 complited

--- SQL/Homework1.sql
+++ SQL/Homework1.sql
@@ -1,3 +1,5 @@
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
 --film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 SELECT title,description From film;
 
